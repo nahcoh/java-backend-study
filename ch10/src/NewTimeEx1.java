24138d415aaed153fabb95ce667e89b9a1d02289
nahcoh <nahcohflow2103@gmail.com>
2025-08-04 15:28:33 +0900

    날짜와 시간 & 형식화 따라치기

--- ch10/src/NewTimeEx1.java
+++ ch10/src/NewTimeEx1.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 public class NewTimeEx1 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();  //오늘의 날짜
+        LocalTime now = LocalTime.now(); //현재 시간
+
+        LocalDate birthDate = LocalDate.of(1996, 03, 21);
+        LocalTime birthTime = LocalTime.of(7, 30, 21);
+
+        System.out.println("today = " + today);
+        System.out.println("now = " + now);
+        System.out.println("birthDate = " + birthDate);
+        System.out.println("birthTime = " + birthTime);
+
+        System.out.println(birthDate.withYear(2000));
+        System.out.println(birthDate.plusDays(1));
+        System.out.println(birthDate.plus(1, ChronoUnit.DAYS));
+
+        // 23:59:59 -> 23:00
+        System.out.println(birthTime.truncatedTo(ChronoUnit.HOURS));
+
+        // 특정 ChronoField의 범위를 알아내는 방법
+        System.out.println(ChronoField.CLOCK_HOUR_OF_DAY.range());  // 1-24
+        System.out.println(ChronoField.HOUR_OF_DAY.range());        // 0-23
+    }
 }
--- ch10/src/NewTimeEx2.java
+++ ch10/src/NewTimeEx2.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.time.*;
 
 public class NewTimeEx2 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2015, 12, 31);
+        LocalTime time = LocalTime.of(12, 34, 56);
+
+        //2015년 12월 31일 12시 34분 56초
+        LocalDateTime dt = LocalDateTime.of(date, time);
+
+        ZoneId zid = ZoneId.of("Asia/Seoul");
+        ZonedDateTime zdt = dt.atZone(zid);
+        String strZid = zdt.getZone().getId();
+
+        ZonedDateTime seoulTime = ZonedDateTime.now();
+        ZoneId nyId = ZoneId.of("America/New_York");
+        ZonedDateTime nyTime = ZonedDateTime.now().withZoneSameInstant(nyId);
+
+        // zonedDatetime -> OffsetDateTime
+        OffsetDateTime odt = zdt.toOffsetDateTime();
+
+        System.out.println(dt);
+        System.out.println(zid);
+        System.out.println(zdt);
+        System.out.println(seoulTime);
+        System.out.println(nyTime);
+        System.out.println(odt);
+
+    }
 }
